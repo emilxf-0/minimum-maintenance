fa3362ca441dbddcecc342d67366b31ea1b35322
Emil Forsén <emilxforsen@gmail.com>
2023-02-04 10:53:49 +0100

    Added pause functionality

--- Minimum Maintenance/Assets/Scripts/UIInputHandler.cs
+++ Minimum Maintenance/Assets/Scripts/UIInputHandler.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.SceneManagement;
 
 public class UIInputHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseMenu;
+    private bool gamePaused;
+    
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            PauseGame();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
@@ -25,6 +36,20 @@ public class UIInputHandler : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void PauseGame()
+    {
+        if (gamePaused == false)
+        {
+            pauseMenu.SetActive(true);
+            gamePaused = true;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            gamePaused = false;
+        }
     }
 
 }
