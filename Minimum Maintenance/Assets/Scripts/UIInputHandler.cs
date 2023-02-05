16cb0b7ee5379e4e251dce88be154eba869ae38b
Risalord <max.hej777@gmail.com>
2023-02-05 17:04:40 +0100

    Added goat eating grass

--- Minimum Maintenance/Assets/Scripts/UIInputHandler.cs
+++ Minimum Maintenance/Assets/Scripts/UIInputHandler.cs
@@ -138,8 +138,8 @@ public class UIInputHandler : MonoBehaviour
     private void SetWinner(bool isLeft)
     {
         Gamepad[] allgamePads = Gamepad.all.ToArray();
-        allgamePads[0].SetMotorSpeeds(0, 0);
-        allgamePads[1].SetMotorSpeeds(0, 0);
+        allgamePads[0]?.SetMotorSpeeds(0, 0);
+        allgamePads[1]?.SetMotorSpeeds(0, 0);
         gameDone = true;
         GameOverPanel.SetActive(true);
         FreezeGame(true);
